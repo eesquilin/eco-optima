Refactor data generation cell to improve clarity and fix anomalies in fuel level calculation

eco-optima.py
@@ -7,7 +7,7 @@ app = marimo.App(width="medium")
 @app.cell
 def _(mo):
     mo.md(r"""
-    1. Imports
+    1. Generate Synthetic Fuel Data
     """)
     return
 
@@ -17,11 +17,8 @@ def _():
     import marimo as mo
     import pandas as pd
     import numpy as np
-    return mo, np, pd
 
 
-@app.cell
-def _(np, pd):
     def generate_sensor_data(n_days = 30):
         np.random.seed(42)
 
@@ -34,10 +31,18 @@ def _(np, pd):
         df = pd.DataFrame({"time stamp": time_index, "fuel level": fuel_level, "pressure": pressure})
 
         # Injecting an Anomaly at the end (Leak)
-        df.loc[700:, "fuel level"] -= np.arrange(20 * 5) #Fuel
+        affected_rows = len(df.loc[700:])
+        df.loc[700:, "fuel level"] -= np.arange(affected_rows) * 5 #Fuel
         df.loc[700:, "pressure"] -= 10 #Pressure
 
         return df
+
+    df = generate_sensor_data()
+    return (mo,)
+
+
+@app.cell
+def _():
     return
 
 